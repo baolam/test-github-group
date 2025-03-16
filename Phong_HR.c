738bb8695d31e02ac0f172263643035fb70d2858
Huy Phong <touman876@gmail.com>
2025-03-16 21:39:55 +0700

    hihi

--- Phong_HR.c
+++ Phong_HR.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello World!");
+    printf("Hello World!\n");
     int vol;
     vol = 2;
-    printf("Chich o Lam bang %d vol", vol);
-    printf("Ngon nhao vo!");
+    printf("Chich o Lam bang %d vol\n", vol);
+    printf("Ngon nhao vo!\n");
+    printf("Ban chac chua?\n");
+
     return 0;
 }
